91fb1b0a6113d2827bf21f8ae02b998f0ef8c87a
developer <developer@developer>
2015-05-04 08:52:08 -0400

    First commit

---
+++ Assets/GotowTutorialCSharp/Scripts/Car Control/WheelAlignment_Script.cs
@@ -0,0 +1,34 @@
+﻿// the visible wheel, the slip prefab is the prefab instantiated when the wheels slide, the rotation value is the
+// value used to rotate the wheel around it's axel.
+
+using UnityEngine;
+using System.Collections;
+
+public class WheelAlignment_Script : MonoBehaviour {
+
+	public WheelCollider CorrespondingCollider;
+	public GameObject SlipPrefab;
+
+	void  Update (){
+				
+		Quaternion rotation;
+		Vector3 position;
+		
+		CorrespondingCollider.GetWorldPose (out position, out rotation);
+		
+		transform.position = position;
+		transform.rotation = rotation;
+	
+		WheelHit CorrespondingGroundHit;
+		CorrespondingCollider.GetGroundHit(out CorrespondingGroundHit );
+		
+		// if the slip of the tire is greater than 0.1f, and the slip prefab exists, create an instance of it on the ground at
+		// a zero rotation.
+		if ( Mathf.Abs( CorrespondingGroundHit.sidewaysSlip ) > 0.1f ) {
+			if ( SlipPrefab ) {
+				Instantiate( SlipPrefab, CorrespondingGroundHit.point, Quaternion.identity );
+			}
+		}
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Unity-CarTutorialProject-Gotow
+==============================
+
+Gotow Unity Car Tutorial from 2009.  
+Upgraded to work with Unity 4.6  
+Maya Models Replaced with FBX Models.  
+AIF Audio Replaced with Ogg Vorbis.  
+
+C# Scripts converted with [M2H's online JS-to-C# converter](http://www.m2h.nl/files/js_to_c.php)
